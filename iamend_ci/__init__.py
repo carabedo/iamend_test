5a8344e8995c23fd63116bbb7cc9f1041a249b54
muydipalma <carabedo@gmail.com>
2022-06-08 13:21:51 -0300

    asdf

---
+++ iamend_ci/__init__.py
@@ -0,0 +1,19 @@
+import iamend_ci.bo as bo
+import iamend_ci.theo as theo
+import iamend_ci.fit as fit
+import iamend_ci.so as so
+import iamend_ci.plt as plt
+import iamend_ci.ax as ax
+
+class exp():
+    def __init__(self):
+        self.hola='si'
+
+
+    def load(self,path):
+
+
+        so.pack(path,bobina,sigmas,espesores)
+
+
+        print(self.hola)    
